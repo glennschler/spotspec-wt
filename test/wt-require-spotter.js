943cd6af367f12aca1363f147624f69a33e26114
glennschler <glennschler@users.noreply.github.com>
2015-08-10 19:24:35 -0300

    updated proof of concept to use the AwsSpotter class from a remote repository

---
+++ test/wt-require-spotter.js
@@ -0,0 +1,112 @@
+const https = require('https');
+const Util = require('util');
+const vm = require('vm');
+const mods = require('module');
+const concat = require('concat-stream');
+
+/**
+* Proof of concept, require a remote module
+*/
+var remoteRequire = function (callback) {
+  var url = 'https://raw.githubusercontent.com/glennschler/aws-spotter-node/master/index.js';
+
+  https.get(url, function(res) {
+    res.setEncoding('utf8');
+    res.pipe(concat({ encoding: 'string' }, function(remoteSrc) {
+      var wrappedSrc = mods.wrap(remoteSrc);
+      vm.runInThisContext(wrappedSrc,
+        'AwsSpotter.js')(null, require, module, __filename, __dirname);
+
+      var moduleClass = module.exports;
+      callback(moduleClass);
+    }));
+  });
+}
+
+module.exports = function(context, cb) {
+  var awsCreds = context.data.wtData; // Secure data set during the 'wt create'
+  awsCreds = parseJson(awsCreds);
+
+  // These run time execution arguments are sent as JSON postdata
+  var runArgs = context.body;
+
+  // EC2 IAM user credentials must exist
+  if (!awsCreds || !awsCreds.accessKeyId || !awsCreds.secretAccessKey) {
+    return cb(new Error('EC2 secrets are missing'));
+  }
+
+  if (typeof runArgs === 'undefined') {
+    return cb(new Error('AwsSpotter arguments are missing'));
+  }
+
+  logInfo(typeof runArgs, runArgs);
+
+  // The EC2 region is sent at execution time. Merge with the EC2 credentials
+  awsCreds.region = !runArgs ? '' : runArgs.region;
+  var instanceType = !runArgs ? '' : runArgs.type;
+
+  // Fetch the AwsSpotter module and then make the the call
+  remoteRequire(function (requiredClass) {
+    const AwsSpotter = requiredClass;
+
+    var spotter = null;
+    try {
+      var isLogging = true;
+      spotter = new AwsSpotter(awsCreds, isLogging);
+    }
+    catch (errMsg) {
+      returnError('Error instantiating AwsSpotter:', errMsg);
+    }
+
+    if (spotter !== null) {
+      spotPrices(spotter);
+    }
+  });
+
+  var spotPrices = function (spotter) {
+    var priceOpts = {
+      type: runArgs.type || 'm3.medium',
+      product: runArgs.product || 'Linux/UNIX',
+      dryRun: runArgs.dryRun
+    };
+
+
+    // Listen for price history results
+    spotter.on('prices', onPrices);
+
+    // Make the price history request
+    spotter.spotPrices(priceOpts);
+  }
+
+  var onPrices = function onPrices(pricesData, err) {
+    if (pricesData === null) {
+      returnError(pricesData);
+    }
+    else {
+      return cb(null, pricesData);
+    }
+  };
+
+  // wrap the error
+  var returnError = function() {
+    errMsg = Util.format.apply(this, arguments);
+    logInfo('Error:', Util.format.apply(this, arguments));
+    return cb(errMsg);
+  }
+};
+
+// helper to parse json
+var parseJson = function(jsonStr) {
+  try {
+    jsonStr = JSON.parse(jsonStr);
+  }
+  catch (parseError) {
+    jsonStr = null;
+  }
+  return jsonStr;
+}
+
+// wrap for future 3rd party logger
+var logInfo = function (message, meta) {
+  console.info(message, meta);
+}
