57797dc69d3bf0e56c708f6cc5eefce534f79805
glennschler <glennschler@users.noreply.github.com>
2015-11-24 19:14:10 -0300

    webpack config. Adjust for node v4 node_modules tree, or node v5 flat structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,32 @@ var webpack = require('webpack')
 
 var nodeModules = {}
 
-fs.readdirSync(path.join(__dirname, './node_modules'))
-  .filter(function (x) {
-    return ['.bin'].indexOf(x) === -1
-  })
-  .forEach(function (mod) {
-    // Do not exclude module 'spotspec'. PACK IT!
-    // since webtask.io does not have in it's module registry
-    if (mod !== 'spotspec') {
-      nodeModules[mod] = 'commonjs ' + mod
-    }
-  })
+// Handle node_modules for npm@3 and npm@2. Flat or tree node_modules
+var recurseExternals = function (curDir, dirName) {
+  var joinedDir = path.join(curDir, dirName)
+  fs.readdirSync(joinedDir)
+    .filter(function (x) {
+      return ['.bin'].indexOf(x) === -1
+    })
+    .forEach(function (mod) {
+      // Do not exclude module 'spotspec'. PACK IT!
+      // since webtask.io does not have in it's module registry
+      if (mod !== 'spotspec') {
+        nodeModules[mod] = 'commonjs ' + mod
+      }
+
+      var treeMods = path.join(joinedDir, mod)
+
+      try {
+        recurseExternals(treeMods, 'node_modules')
+      } catch (err) {
+        // nop. The top of the tree has been reached
+      }
+    })
+}
+
+// recurse through the node_modules tree (or not if node v5 flat structure)
+recurseExternals(__dirname, 'node_modules')
 
 var wtName = 'wt-spotter'
 var libDir = path.join(__dirname, 'lib')
